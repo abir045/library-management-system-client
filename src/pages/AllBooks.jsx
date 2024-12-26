a1639c80242cee6b6a0dc5e5ae5cf1688f685e24
unknown <abir045@gmail.com>
2024-12-26 14:09:11 +0600

    updated filter functionality on allbooks page

--- src/pages/AllBooks.jsx
+++ src/pages/AllBooks.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import BookCard from "../components/BookCard";
 
 const AllBooks = () => {
   const loadedBooks = useLoaderData();
+  const [showAvailable, setShowAvailable] = useState(false);
+
+  console.log(loadedBooks);
+
+  const displayedBooks = showAvailable
+    ? loadedBooks.filter((book) => book.Quantity > 0)
+    : loadedBooks;
+
+  const toggleAvailableBooks = () => {
+    setShowAvailable(!showAvailable);
+  };
+
   return (
     <div>
       <h2 className="text-center text-3xl mt-20 font-bold">All Books</h2>
 
+      <div className="flex justify-center mt-10">
+        <button
+          onClick={toggleAvailableBooks}
+          className=" btn btn-neutral  text-white px-4 py-2 rounded-md transition duration-300"
+        >
+          {showAvailable ? "Show All Books" : "Show Available Books"}
+        </button>
+      </div>
+
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-[5%] mt-10">
-        {loadedBooks.map((book) => (
+        {displayedBooks.map((book) => (
           <BookCard key={book._id} book={book} />
         ))}
       </div>
